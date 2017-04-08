bbd7c84addef6af051f2c7aa4c1e5891a1f1a9e3
John DeLong <jdelong@michiganlabs.com>
2017-04-08 09:34:56 -0400

    Give the computers some smarts

--- DeckOfCards/StrategyEngine.swift
+++ DeckOfCards/StrategyEngine.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class StrategyEngine {
-    
+
     var teams = [[Player]]()
 
     /**
@@ -21,6 +21,71 @@ class StrategyEngine {
     static func determineCardToPlay(from hand: [Card]) -> Card? {
         guard !hand.isEmpty else { return nil }
 
-        return hand.first
+        let cardsInPlay = GameManager.shared.cardsInPlay
+        if let firstCard = cardsInPlay.first {
+
+            if
+                let winningCard = self.getHighestCard(from: cardsInPlay, of: firstCard.suit),
+                let card = self.card(from: hand, thatBeats: winningCard)
+            {
+                return card
+            }
+
+            return self.getLowestCard(from: hand, of: firstCard.suit) ?? self.getLowestCard(from: hand)
+        } else {
+            // Player is leading
+            return self.getHighestCard(from: hand)
+        }
+    }
+
+    static private func card(from cards: [Card], thatBeats highCard: Card) -> Card? {
+        var minCard: Card?
+
+        cards.forEach {
+            if $0.suit == highCard.suit
+                && highCard.compare($0) == .orderedAscending
+                // swiftlint:disable:next opening_brace
+                && ( minCard?.compare($0) == .orderedDescending || minCard == nil )
+            {
+                minCard = $0
+            }
+        }
+
+        return minCard
+    }
+
+    static private func getHighestCard(from cards: [Card], of suit: Card.Suit? = nil) -> Card? {
+        guard var highCard = cards.first else { return nil }
+
+        cards.forEach {
+            if let suit = suit {
+                if $0.suit == suit && highCard.compare($0) == .orderedAscending {
+                    highCard = $0
+                }
+            } else {
+                if highCard.compare($0) == .orderedAscending {
+                    highCard = $0
+                }
+            }
+
+        }
+        return highCard
+    }
+
+    static private func getLowestCard(from cards: [Card], of suit: Card.Suit? = nil) -> Card? {
+        guard var lowCard = cards.first else { return nil }
+
+        cards.forEach {
+            if let suit = suit {
+                if $0.suit == suit && lowCard.compare($0) == .orderedDescending {
+                    lowCard = $0
+                }
+            } else {
+                if lowCard.compare($0) == .orderedDescending {
+                    lowCard = $0
+                }
+            }
+        }
+        return lowCard
     }
 }
